f5b31759c166436fb80a1c454dc02220505da0ef
TomPrint <tomekklewicki@gmail.com>
2024-06-27 22:54:41 +0200

    initialize panellum

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service'; 
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'; 
+
+declare var pannellum: any;
 
 @Component({
   selector: 'app-home',
@@ -15,8 +17,18 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('panoramaContainer') panoramaContainer!: ElementRef;
+
   constructor(private authService: AuthService, private router: Router) {}
+
+  ngAfterViewInit(): void {
+    pannellum.viewer(this.panoramaContainer.nativeElement, {
+      type: "equirectangular",
+      panorama: "/assets/360image.jpg",
+      autoLoad: true
+    });
+  }
 
   logout(): void {
     this.authService.logout().then(() => {
