8eb7b9e3764dfc86143a20b6bf161562d6734474
TomPrint <tomekklewicki@gmail.com>
2024-07-02 22:32:01 +0200

    basic scene layout with panellum

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -24,11 +24,50 @@ export class HomeComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     pannellum.viewer(this.panoramaContainer.nativeElement, {
-      type: "equirectangular",
-      panorama: "/assets/360image.jpg",
-      autoLoad: true
+        "default": {
+            "firstScene": "city",
+            "author": "Angular Virtual Tour",
+            "sceneFadeDuration": 1000
+        },
+        "scenes": {
+            "city": {
+                "title": "Sleepy Hollow",
+                "hfov": 110,
+                "pitch": -3,
+                "yaw": 117,
+                "type": "equirectangular",
+                "panorama": "https://i.imgur.com/7HLdNDE.jpeg",
+                "hotSpots": [
+                    {
+                        "pitch": -4.0,
+                        "yaw": 2490.00,
+                        "type": "scene",
+                        "text": "Hudson River",
+                        "sceneId": "river"
+                    }
+                ]
+            },
+            "river": {
+                "title": "Hudson River",
+                "hfov": 110,
+                "yaw": 5,
+                "type": "equirectangular",
+                "panorama": "https://i.imgur.com/mRGAw5K.jpeg",
+                "hotSpots": [
+                    {
+                        "pitch": 0.0,
+                        "yaw": 2349.0,
+                        "type": "scene",
+                        "text": "Sleepy Holow",
+                        "sceneId": "city",
+                        "targetYaw": -23,
+                        "targetPitch": 2
+                    }
+                ]
+            }
+        }
     });
-  }
+}
 
   logout(): void {
     this.authService.logout().then(() => {
